2c3cf0a70dee638fa59f89d3e12cae28ecb7d8a6
chris.ruel <chris.ruel@lfg.com>
2020-02-18 11:28:43 -0500

    Adding script to get instance info

---
+++ rds-info/get-rds-info.py
@@ -0,0 +1,22 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='default')
+rds = session.resource('rds')
+
+@click.command()
+def describe_db_instances():
+    "Describe RDS instances"
+    for i in rds.describe_db_instances.all():
+           print (', '.join((
+               i.DBInstanceIdentifier,
+               i.DBInstanceClass,
+               i.Engine,
+               i.DBInstanceStatus,
+               i.DBName,
+               i.Endpoint)))
+
+    return
+
+if __name__ == '__main__':
+   describe_db_instances()
