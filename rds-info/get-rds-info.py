75917af12cc3632b6e9f7851b686b204d3dba5a7
chris.ruel <chris.ruel@lfg.com>
2020-02-18 11:49:10 -0500

    syntax fixes

--- rds-info/get-rds-info.py
+++ rds-info/get-rds-info.py
@@ -2,19 +2,20 @@ import boto3
 import click
 
 session = boto3.Session(profile_name='default')
-client = boto3.client('rds')
+rds = boto3.client('rds')
+dbs = rds.describe_db_instances()
 
 @click.command()
 def describe_db_instances():
     "Describe RDS instances"
-    for i in client.describe_db_instances():
+    for i in dbs['DBInstances']:
            print (', '.join((
-               i.DBInstanceIdentifier,
-               i.DBInstanceClass,
-               i.Engine,
-               i.DBInstanceStatus,
-               i.DBName,
-               i.Endpoint)))
+               i['DBInstanceIdentifier'],
+               i['DBInstanceClass'],
+               i['Engine'],
+               i['DBInstanceStatus'],
+               i['DBName'],
+               i['Endpoint']['Address'])))
 
     return
 
