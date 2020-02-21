f7c6c1382116646f8978142e7313740ef68a536b
chris.ruel <chris.ruel@lfg.com>
2020-02-21 09:30:26 -0500

    Adding start/stop functionality

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,12 +4,28 @@ import click
 session = boto3.Session(profile_name='python-aws')
 ec2 = session.resource('ec2')
 
-@click.command()
+def filter_instances(project):
+    instances=[]
+
+    if project:
+        filters = [{'Name':'tag:project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+    
+    return instances
+
+@click.group()
+def instances():
+    """Commands for instances"""
+
+@instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
-    instances=[]
+
+    instances=filter_instances(project)
 
     if project:
         filters = [{'Name':'tag:project', 'Values':[project]}]
@@ -30,5 +46,33 @@ def list_instances(project):
 
     return
 
+@instances.command('stop')
+@click.option('--project', default=None,
+   help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 instance"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+   help='Only instances for project')
+def start_instances(project):
+    "Start EC2 instance"
+
+    instances=filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+
+    return
+    
 if __name__ == '__main__':
-   list_instances()
+   instances()
