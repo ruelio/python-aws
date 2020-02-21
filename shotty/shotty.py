fa0626f6732610ef35e8ec5238f3f610ba5817ba
chris.ruel <chris.ruel@lfg.com>
2020-02-21 16:39:05 -0500

    Adding stop/start to snapshot command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -82,9 +82,21 @@ def create_snapshot(project):
     instances=filter_instances(project)
 
     for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped_all()
+        
         for v in i.volumes.all():
             print("Creating snapshot of {0}".format(v.id))
             v.create_snapshot(Description="Created by Shotty")
+        
+        print("Starting {0}...".format(i.id))
+        
+        i.start()
+        i.wait_until_running()
+    
+    print("Job's done!")
     
     return
     
